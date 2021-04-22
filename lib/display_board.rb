f884f31fb34731528de829da3276ee60235da359
Patrick Calderon <calderpat@gmail.com>
2021-04-22 20:48:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,12 @@
-puts "Welcome to Tic Tac Toe!"
+def display_board
+  
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+end
+
+puts "Welcome to Tic Tac Toe!"
+
+display_board
